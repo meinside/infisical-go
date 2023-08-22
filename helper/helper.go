841ba34848fbae694f37da9586abeb4ac7147f82
Sungjin Han <meinside@meinside.dev>
2023-08-22 12:14:43 +0900

    Add helper functions for retrieving multiple values

--- helper/helper.go
+++ helper/helper.go
@@ -1,6 +1,10 @@
 package helper
 
-import "github.com/meinside/infisical-go"
+import (
+	"errors"
+
+	"github.com/meinside/infisical-go"
+)
 
 // Value returns the secret value for given parameters.
 //
@@ -16,6 +20,33 @@ func Value(workspaceID, token, environment string, secretType infisical.SecretTy
 	return client.RetrieveSecretValue(workspaceID, environment, secretType, secretKeyPath)
 }
 
+// Values returns the secret values for given parameters.
+//
+// Works only on E2EE-disabled workspaces.
+func Values(workspaceID, token, environment string, secretType infisical.SecretType, secretKeyPaths []string) (map[string]string, error) {
+	client := infisical.NewClientWithoutAPIKey(map[string]infisical.WorkspaceToken{
+		workspaceID: {
+			Token: token,
+			E2EE:  false,
+		},
+	})
+
+	var value string
+	var err error
+	values := map[string]string{}
+	errs := []error{}
+	for _, secretKeyPath := range secretKeyPaths {
+		value, err = client.RetrieveSecretValue(workspaceID, environment, secretType, secretKeyPath)
+		if err == nil {
+			values[secretKeyPath] = value
+		} else {
+			errs = append(errs, err)
+		}
+	}
+
+	return values, errors.Join(errs...)
+}
+
 // E2EEValue returns the decrypted secret value for given parameters.
 //
 // Works only on E2EE-enabled workspaces.
@@ -28,4 +59,31 @@ func E2EEValue(apiKey, workspaceID, token, environment string, secretType infisi
 	})
 
 	return client.RetrieveSecretValue(workspaceID, environment, secretType, secretKeyPath)
+}
+
+// E2EEValue returns the decrypted secret values for given parameters.
+//
+// Works only on E2EE-enabled workspaces.
+func E2EEValues(workspaceID, token, environment string, secretType infisical.SecretType, secretKeyPaths []string) (map[string]string, error) {
+	client := infisical.NewClientWithoutAPIKey(map[string]infisical.WorkspaceToken{
+		workspaceID: {
+			Token: token,
+			E2EE:  true,
+		},
+	})
+
+	var value string
+	var err error
+	values := map[string]string{}
+	errs := []error{}
+	for _, secretKeyPath := range secretKeyPaths {
+		value, err = client.RetrieveSecretValue(workspaceID, environment, secretType, secretKeyPath)
+		if err == nil {
+			values[secretKeyPath] = value
+		} else {
+			errs = append(errs, err)
+		}
+	}
+
+	return values, errors.Join(errs...)
 }
