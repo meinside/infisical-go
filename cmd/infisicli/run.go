4e77abfe9847acb5d2f7546469416e03cb7276ed
Sungjin Han <meinside@meinside.dev>
2023-08-25 11:18:18 +0900

    Add features: create/update/delete a secret

--- cmd/infisicli/run.go
+++ cmd/infisicli/run.go
@@ -8,6 +8,7 @@ import (
 	"path"
 	"path/filepath"
 	"regexp"
+	"strings"
 
 	"github.com/meinside/infisical-go"
 	"github.com/meinside/version-go"
@@ -20,20 +21,24 @@ const (
 
 const (
 	// arguments for commands
-	argHelpShort              = "-h"
-	argHelpLong               = "--help"
-	argListOrganizationsShort = "-lo"
-	argListOrganizationsLong  = "--list-organizations"
-	argListWorkspacesShort    = "-lw"
-	argListWorkspacesLong     = "--list-workspaces"
-	argListAllSecretsShort    = "-las"
-	argListAllSecretsLong     = "--list-all-secrets"
-	argPrintValueShort        = "-p"
-	argPrintValueLong         = "--print"
-	argVersionShort           = "-v"
-	argVersionLong            = "--version"
-	argVerboseShort           = "-V"
-	argVerboseLong            = "--verbose"
+	cmdHelpShort              = "-h"
+	cmdHelpLong               = "--help"
+	cmdListOrganizationsShort = "-lo"
+	cmdListOrganizationsLong  = "--list-organizations"
+	cmdListWorkspacesShort    = "-lw"
+	cmdListWorkspacesLong     = "--list-workspaces"
+	cmdListAllSecretsShort    = "-las"
+	cmdListAllSecretsLong     = "--list-all-secrets"
+	cmdPrintValueShort        = "-p"
+	cmdPrintValueLong         = "--print"
+	cmdNewValueShort          = "-n"
+	cmdNewValueLong           = "--new-value"
+	cmdUpdateValueShort       = "-u"
+	cmdUpdateValueLong        = "--update-value"
+	cmdDeleteValueShort       = "-d"
+	cmdDeleteValueLong        = "--delete-value"
+	cmdVersionShort           = "-v"
+	cmdVersionLong            = "--version"
 
 	// arguments for command parameters
 	argOrganizationShort = "-o"
@@ -46,8 +51,16 @@ const (
 	argTypeLong          = "--type"
 	argKeyShort          = "-k"
 	argKeyLong           = "--key"
+	argSecretShort       = "-s"
+	argSecretLong        = "--secret"
+	argCommentShort      = "-c"
+	argCommentLong       = "--comment"
 	argFolderShort       = "-f"
 	argFolderLong        = "--folder"
+
+	// arguments for something else
+	argVerboseShort = "-V"
+	argVerboseLong  = "--verbose"
 
 	// regex for parsing command param arguments (in 'key=value' format)
 	regexKeyValue = `(.*?)=['"]?(.*?)['"]?$`
@@ -118,53 +131,76 @@ func showHelp(err error) {
 	fmt.Printf(`
 Usage:
 
-	%[1]s %[2]s
-	%[1]s %[3]s
-	: print this help message to stdout.
+  %[1]s %[2]s
+  %[1]s %[3]s
+  : print this help message to stdout.
 
-	%[1]s %[4]s
-	%[1]s %[5]s
-	: print the version string to stdout.
+  %[1]s %[4]s
+  %[1]s %[5]s
+  : print the version string to stdout.
 
-	%[1]s %[6]s
-	%[1]s %[7]s
-	: list all your organizations to stdout. (needed: 'api_key' and 'token')
+  %[1]s %[6]s
+  %[1]s %[7]s
+  : list all your organizations to stdout. (needed: 'api_key' and 'token')
 
-	%[1]s %[8]s
-	%[1]s %[9]s
-	: list all your workspaces to stdout. (needed: 'api_key' and 'token')
-		eg. %[1]s %[8]s %[14]s=0a1b2c3d4e5f
-		eg. %[1]s %[9]s %[15]s=0a1b2c3d4e5f
+  %[1]s %[8]s
+  %[1]s %[9]s
+  : list all your workspaces to stdout. (needed: 'api_key' and 'token')
+    eg. %[1]s %[8]s %[20]s=0a1b2c3d4e5f
+    eg. %[1]s %[9]s %[21]s=0a1b2c3d4e5f
 
-	%[1]s %[10]s
-	%[1]s %[11]s
-	: list all secret values in a given folder (default: /) to stdout. (only 'token' is needed when E2EE is disabled)
-		eg. %[1]s %[10]s %[18]s=012345abcdefg %[20]s=dev
-		eg. %[1]s %[11]s %[19]s=012345abcdefg %[21]s=dev
-		eg. %[1]s %[10]s %[18]s=012345abcdefg %[20]s=dev %[16]s=/folder1/folder2
-		eg. %[1]s %[11]s %[19]s=012345abcdefg %[21]s=dev %[17]s=/folder1/folder2
+  %[1]s %[10]s
+  %[1]s %[11]s
+  : list all secret values in a given folder (default: /) to stdout. (only 'token' is needed when E2EE is disabled)
+    eg. %[1]s %[10]s %[24]s=012345abcdefg %[26]s=dev
+    eg. %[1]s %[11]s %[25]s=012345abcdefg %[27]s=dev
+    eg. %[1]s %[10]s %[24]s=012345abcdefg %[26]s=dev %[16]s=/folder1/folder2
+    eg. %[1]s %[11]s %[25]s=012345abcdefg %[27]s=dev %[17]s=/folder1/folder2
 
-	%[1]s %[12]s
-	%[1]s %[13]s
-	: print the secret value to stdout without a trailing newline. (only 'token' is needed when E2EE is disabled)
-		eg. %[1]s %[12]s %[18]s=012345abcdefg %[20]s=dev %[22]s=shared %[24]s=/folder1/folder2/SECRET_KEY_1
-		eg. %[1]s %[13]s %[19]s=012345abcdefg %[21]s=dev %[23]s=shared %[25]s=/folder1/folder2/SECRET_KEY_1
+  %[1]s %[12]s
+  %[1]s %[13]s
+  : print the secret value to stdout without a trailing newline. (only 'token' is needed when E2EE is disabled)
+    eg. %[1]s %[12]s %[24]s=012345abcdefg %[26]s=dev %[28]s=shared %[30]s=/folder/SECRET_KEY_1
+    eg. %[1]s %[13]s %[25]s=012345abcdefg %[27]s=dev %[29]s=shared %[31]s=/folder/SECRET_KEY_1
+
+  %[1]s %[14]s
+  %[1]s %[15]s
+  : create a new secret with given parameters.
+    eg. %[1]s %[14]s %[24]s=012345abcdefg %[26]s=dev %[28]s=shared %[30]s=/folder/NEW_KEY %[32]s=NEW_VALUE
+    eg. %[1]s %[15]s %[25]s=012345abcdefg %[27]s=dev %[29]s=shared %[31]s=/folder/NEW_KEY %[33]s=NEW_VALUE
+    eg. %[1]s %[14]s %[24]s=012345abcdefg %[26]s=dev %[28]s=shared %[30]s=/folder/NEW_KEY %[32]s=NEW_VALUE %[34]s=COMMENT
+    eg. %[1]s %[15]s %[25]s=012345abcdefg %[27]s=dev %[29]s=shared %[31]s=/folder/NEW_KEY %[33]s=NEW_VALUE %[35]s=COMMENT
+
+  %[1]s %[16]s
+  %[1]s %[17]s
+  : update the value of a secret at a given key-path.
+    eg. %[1]s %[16]s %[24]s=012345abcdefg %[26]s=dev %[28]s=shared %[30]s=/folder/SOME_KEY %[32]s=UPDATED_VALUE
+    eg. %[1]s %[17]s %[25]s=012345abcdefg %[27]s=dev %[29]s=shared %[31]s=/folder/SOME_KEY %[33]s=UPDATED_VALUE
+
+  %[1]s %[18]s
+  %[1]s %[19]s
+  : delete a secret at a given key-path.
+    eg. %[1]s %[18]s %[24]s=012345abcdefg %[26]s=dev %[28]s=shared %[30]s=/folder/SOME_KEY
+    eg. %[1]s %[19]s %[25]s=012345abcdefg %[27]s=dev %[29]s=shared %[31]s=/folder/SOME_KEY
 
 Other optional arguments:
 
-	%[26]s / %[27]s
-	: dump http requests/responses for debugging.
+  %[36]s / %[37]s
+  : dump http requests/responses for debugging.
 `,
 		// executable name
 		applicationName,
 
 		// commands
-		argHelpShort, argHelpLong,
-		argVersionShort, argVersionLong,
-		argListOrganizationsShort, argListOrganizationsLong,
-		argListWorkspacesShort, argListWorkspacesLong,
-		argListAllSecretsShort, argListAllSecretsLong,
-		argPrintValueShort, argPrintValueLong,
+		cmdHelpShort, cmdHelpLong,
+		cmdVersionShort, cmdVersionLong,
+		cmdListOrganizationsShort, cmdListOrganizationsLong,
+		cmdListWorkspacesShort, cmdListWorkspacesLong,
+		cmdListAllSecretsShort, cmdListAllSecretsLong,
+		cmdPrintValueShort, cmdPrintValueLong,
+		cmdNewValueShort, cmdNewValueLong,
+		cmdUpdateValueShort, cmdUpdateValueLong,
+		cmdDeleteValueShort, cmdDeleteValueLong,
 
 		// parameters
 		argOrganizationShort, argOrganizationLong,
@@ -173,6 +209,8 @@ Other optional arguments:
 		argEnvironmentShort, argEnvironmentLong,
 		argTypeShort, argTypeLong,
 		argKeyShort, argKeyLong,
+		argSecretShort, argSecretLong,
+		argCommentShort, argCommentLong,
 
 		// others
 		argVerboseShort, argVerboseLong,
@@ -220,21 +258,27 @@ func run(args []string) {
 	verbose := hasArg(args, argVerboseShort, argVerboseLong)
 
 	var err error
-	if hasArg(args, argVersionShort, argVersionLong) {
+	if hasArg(args, cmdVersionShort, cmdVersionLong) {
 		// show version
 		showVersion()
-	} else if hasArg(args, argHelpShort, argHelpLong) {
+	} else if hasArg(args, cmdHelpShort, cmdHelpLong) {
 		// do nothing
 	} else {
 		//handle commands here
-		if hasArg(args, argListOrganizationsShort, argListOrganizationsLong) {
+		if hasArg(args, cmdListOrganizationsShort, cmdListOrganizationsLong) {
 			err = doListOrganizations(verbose)
-		} else if hasArg(args, argListWorkspacesShort, argListWorkspacesLong) {
+		} else if hasArg(args, cmdListWorkspacesShort, cmdListWorkspacesLong) {
 			err = doListWorkspaces(args, verbose)
-		} else if hasArg(args, argListAllSecretsShort, argListAllSecretsLong) {
+		} else if hasArg(args, cmdListAllSecretsShort, cmdListAllSecretsLong) {
 			err = doListAllSecrets(args, verbose)
-		} else if hasArg(args, argPrintValueShort, argPrintValueLong) {
+		} else if hasArg(args, cmdPrintValueShort, cmdPrintValueLong) {
 			err = doPrintValue(args, verbose)
+		} else if hasArg(args, cmdNewValueShort, cmdNewValueLong) {
+			err = doCreateValue(args, verbose)
+		} else if hasArg(args, cmdUpdateValueShort, cmdUpdateValueLong) {
+			err = doUpdateValue(args, verbose)
+		} else if hasArg(args, cmdDeleteValueShort, cmdDeleteValueLong) {
+			err = doDeleteValue(args, verbose)
 		}
 	}
 
@@ -445,6 +489,148 @@ func doPrintValue(args []string, verbose bool) error {
 		result, err = c.RetrieveSecretValue(workspace, environment, infisical.SecretType(typ), key)
 		if err == nil {
 			fmt.Printf("%s", result)
+
+			os.Exit(0)
+		}
+
+		return err
+	}, verbose)
+}
+
+// create a new secret value, will os.Exit(0) on success
+func doCreateValue(args []string, verbose bool) error {
+	return do(func(c *infisical.Client) error {
+		var err error
+		params := convertKeyValueParams(args)
+
+		var key, value, workspace, environment, typ, comment string
+		if key, err = valueFromKVs(argKeyShort, argKeyLong, params); err != nil {
+			return err
+		}
+		if value, err = valueFromKVs(argSecretShort, argSecretLong, params); err != nil {
+			return err
+		}
+		if workspace, err = valueFromKVs(argWorkspaceShort, argWorkspaceLong, params); err != nil {
+			return err
+		}
+		if environment, err = valueFromKVs(argEnvironmentShort, argEnvironmentLong, params); err != nil {
+			return err
+		}
+		if typ, err = valueFromKVs(argTypeShort, argTypeLong, params); err != nil {
+			return err
+		}
+		comment, _ = valueFromKVs(argCommentShort, argCommentLong, params)
+
+		// key => folder + secretKey
+		splitted := strings.Split(key, "/")
+		secretKey := splitted[len(splitted)-1]
+		folder := strings.TrimSuffix(key, secretKey)
+
+		// request params
+		createParams := infisical.NewParamsCreateSecret().
+			SetType(infisical.SecretType(typ))
+		if folder != "" {
+			createParams.SetSecretPath(folder)
+		}
+		if comment != "" {
+			createParams.SetSecretComment(comment)
+		}
+
+		// create
+		err = c.CreateSecret(workspace, environment, secretKey, value, createParams)
+		if err == nil {
+			fmt.Printf("> Successfully created a new secret value at: %s\n", key)
+
+			os.Exit(0)
+		}
+
+		return err
+	}, verbose)
+}
+
+// update a secret value, will os.Exit(0) on success
+func doUpdateValue(args []string, verbose bool) error {
+	return do(func(c *infisical.Client) error {
+		var err error
+		params := convertKeyValueParams(args)
+
+		var key, value, workspace, environment, typ string
+		if key, err = valueFromKVs(argKeyShort, argKeyLong, params); err != nil {
+			return err
+		}
+		if value, err = valueFromKVs(argSecretShort, argSecretLong, params); err != nil {
+			return err
+		}
+		if workspace, err = valueFromKVs(argWorkspaceShort, argWorkspaceLong, params); err != nil {
+			return err
+		}
+		if environment, err = valueFromKVs(argEnvironmentShort, argEnvironmentLong, params); err != nil {
+			return err
+		}
+		if typ, err = valueFromKVs(argTypeShort, argTypeLong, params); err != nil {
+			return err
+		}
+
+		// key => folder + secretKey
+		splitted := strings.Split(key, "/")
+		secretKey := splitted[len(splitted)-1]
+		folder := strings.TrimSuffix(key, secretKey)
+
+		// request params
+		updateParams := infisical.NewParamsUpdateSecret().
+			SetType(infisical.SecretType(typ))
+		if folder != "" {
+			updateParams.SetSecretPath(folder)
+		}
+
+		// update
+		err = c.UpdateSecret(workspace, environment, secretKey, value, updateParams)
+		if err == nil {
+			fmt.Printf("> Successfully updated a secret value at: %s\n", key)
+
+			os.Exit(0)
+		}
+
+		return err
+	}, verbose)
+}
+
+// delete a secret value, will os.Exit(0) on success
+func doDeleteValue(args []string, verbose bool) error {
+	return do(func(c *infisical.Client) error {
+		var err error
+		params := convertKeyValueParams(args)
+
+		var key, workspace, environment, typ string
+		if key, err = valueFromKVs(argKeyShort, argKeyLong, params); err != nil {
+			return err
+		}
+		if workspace, err = valueFromKVs(argWorkspaceShort, argWorkspaceLong, params); err != nil {
+			return err
+		}
+		if environment, err = valueFromKVs(argEnvironmentShort, argEnvironmentLong, params); err != nil {
+			return err
+		}
+		if typ, err = valueFromKVs(argTypeShort, argTypeLong, params); err != nil {
+			return err
+		}
+
+		// key => folder + secretKey
+		splitted := strings.Split(key, "/")
+		secretKey := splitted[len(splitted)-1]
+		folder := strings.TrimSuffix(key, secretKey)
+
+		// request params
+		deleteParams := infisical.NewParamsDeleteSecret().
+			SetType(infisical.SecretType(typ))
+		if folder != "" {
+			deleteParams.SetSecretPath(folder)
+		}
+
+		// delete
+		err = c.DeleteSecret(workspace, environment, secretKey, deleteParams)
+		if err == nil {
+			fmt.Printf("> Successfully deleted a secret value at: %s\n", key)
 
 			os.Exit(0)
 		}
